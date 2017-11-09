b3e416a5bcf2b6ca8a087f2e059a589166952ece
Roman S <roman@itech.ua>
2017-11-08 18:37:16 -0800

    add DFS BFS

---
+++ data-structures/heap.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+"""Search triangles in graph"""
+__author__ = "Roman S"
+__copyright__ = "Copyleft"
+
+import sys
+import numpy as np
+
+class BinaryHeap:
+    def __init__(self):
+        self.heap = [0]
+        self.heapSize = 0
+
+    @classmethod
+    def fromlist(self, items: list):        
+        instance = self()        
+        i = len(items) // 2
+        instance.heapSize = len(items)
+        instance.heap = [0] + items[:]
+        while (i > 0):
+            self.percDown(instance, i)
+            i = i - 1
+        return instance
+
+    def __add__(self, heap):
+        if isinstance(heap, BinaryHeap):
+            return BinaryHeap.fromlist(list(self.mergelists(heap.heap[1:], self.heap[1:])))
+        return NotImplemented
+
+    def mergelists(self, list1, list2):
+        if len(list1) is 0:
+            return list2
+        if len(list2) is 0:
+            return list1
+        if list1[0] < list2[0]:
+            return np.append(list1[0], self.mergelists(list1[1:], list2))
+        else:
+            return np.append(list2[0], self.mergelists(list1, list2[1:]))
+
+
+    def percUp(self, i):
+        while i // 2 > 0:
+            print(self.heap)
+            if self.heap[i] < self.heap[i // 2]:
+                tmp = self.heap[i // 2]
+                self.heap[i // 2] = self.heap[i]
+                self.heap[i] = tmp
+            i = i // 2
+
+    def percDown(self,i):
+        while (i * 2) <= self.heapSize:
+            mc = self.minChild(i)
+            if self.heap[i] > self.heap[mc]:
+                tmp = self.heap[i]
+                self.heap[i] = self.heap[mc]
+                self.heap[mc] = tmp
+            i = mc
+
+    def minChild(self,i):
+        if i * 2 + 1 > self.heapSize:
+            return i * 2
+        else:
+            if self.heap[i*2] < self.heap[i*2+1]:
+                return i * 2
+            else:
+                return i * 2 + 1
+
+    def pop(self):
+        retval = self.heap[1]
+        self.heap[1] = self.heap[self.heapSize]
+        self.heapSize = self.heapSize - 1
+        self.heap.pop()
+        self.percDown(1)
+        return retval
+
+    def push(self, item):
+        self.heap.append(item)
+        self.heapSize = self.heapSize + 1
+        self.percUp(self.heapSize)
+
+    def peek(self):
+        return self.heap[1]
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        print('Nope args')
+
+    bh1 = BinaryHeap.fromlist([9,5,6,2,3,17])    
+    bh2 = BinaryHeap.fromlist([10,1,18,9,30,19])    
+    bh = bh1 + bh2
+    #print(bh1.merge(bh2))
+    
+    bh.push(11)
+
+    print(bh.peek())
+    print(bh.pop())
+    print(bh.pop())
+    print(bh.pop())
+    print(bh.pop())
+    print(bh.pop())
+    print(bh.pop())
+    print(bh.pop())
+    print(bh.pop())
+    print(bh.pop())
---
+++ graphs/dfs-bfs.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+"""DFS BFS"""
+__author__ = "Roman S"
+__copyright__ = "Copyleft"
+
+import sys
+from collections import deque
+
+class Node:
+    def __init__(self, id):
+        self.id = id
+        self.adjacent = list()  #linkedlist
+    
+    def addAdjacentNode(self, id_node):
+        self.adjacent.append(id_node)
+
+class Graph:
+    def __init__(self):
+        self.nodes = dict()
+
+    def addEdge(self, src, dst):
+        if not src in self.nodes:
+            self.nodes[src] = Node(src)
+        
+        if not dst in self.nodes:
+            self.nodes[dst] = Node(dst)
+
+        self.nodes[src].addAdjacentNode(dst)
+        self.nodes[dst].addAdjacentNode(src)
+    
+    def getNode(self, id):
+        return self.nodes[id]
+
+class GraphSearch:
+
+    def __init__(self, graph):
+        self.graph = graph    
+    
+
+    def dfs(self, src, dst):
+        if not src in self.graph.nodes or not dst in self.graph.nodes :
+            return False
+        visited = list()
+        return self.hasPathDFS(src, dst, visited)
+
+    
+    def hasPathDFS(self, src, dst, visited):        
+        if src in visited:
+            return False
+        visited.append(src)
+        if src == dst:            
+            return True        
+        for child in self.graph.getNode(src).adjacent:            
+            if(self.hasPathDFS(child, dst, visited)):
+                return True
+        return False
+
+
+    def bfs(self, src, dst):
+        if not src in self.graph.nodes or not dst in self.graph.nodes :
+            return False
+        nextToVisit = list()  # linkedlist
+        visited = list()     # hashset
+        nextToVisit.append(src)
+        while len(nextToVisit) > 0:
+            node = nextToVisit.pop()
+            if node == dst:
+                return True
+
+            if node in visited:
+                continue
+
+            visited.append(node)
+
+            for child in self.graph.getNode(node).adjacent:
+                nextToVisit.append(child)
+        return False
+
+
+
+
+    
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        print('Nope args')
+
+    graph = Graph()
+    graph.addEdge(1, 2)
+    graph.addEdge(2, 3)
+    graph.addEdge(3, 4)
+    graph.addEdge(2, 5)
+    graph.addEdge(20, 50)
+
+    print(GraphSearch(graph).dfs(1, 4))
+    print(GraphSearch(graph).bfs(1, 4))
+
+    
